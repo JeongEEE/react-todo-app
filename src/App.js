7f4ede41a498c0e5bba2653f92c912ec758fa4db
Gyu Jeong Kim <gjkim@beyless.com>
2023-01-31 14:09:34 +0900

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import './App.css';
-import List from './components/List'
+import Lists from './components/Lists'
 import Form from './components/Form'
+
+const initialTodoData = localStorage.getItem('todoData') 
+	? JSON.parse(localStorage.getItem('todoData')) 
+	: [];
 
 // 함수형 컴포넌트
 export default function App() {
 	// npm install
   // npm run start
 
-	const [ todoData, setTodoData ] = useState([]);
+	const [ todoData, setTodoData ] = useState(initialTodoData);
 	const [ value, setValue ] = useState('');
 
   const handleSubmit = (e) => {
@@ -23,17 +27,25 @@ export default function App() {
     };
 
 		setTodoData(prev => [...prev, newTodo]);
+		localStorage.setItem('todoData', JSON.stringify([...todoData, newTodo]))
 		setValue('');
   }
+
+	const handleRemoveAllItems = () => {
+		setTodoData([]);
+		localStorage.setItem('todoData', JSON.stringify([]))
+	}
 
   return(
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+					<button className="p-1 border rounded"
+						onClick={handleRemoveAllItems}> Delete All</button>
         </div>
 				
-				<List todoData={todoData} setTodoData={setTodoData} />
+				<Lists todoData={todoData} setTodoData={setTodoData} />
 
 				<Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { useState } from 'react'
 
-export default function List({ todoData, setTodoData }) {
+// React.memo는 너무자주 랜더링되는것을 최적화 해준다
+const List = React.memo(({
+	id, title, completed, todoData, setTodoData, provided, snapshot
+}) => {
 
-  const handleClick = (id) => {
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedTitle, setEditedTitle] = useState(title);
+
+	const handleClick = (id) => {
     let newTodoData = todoData.filter(data => data.id !== id);
 		setTodoData(newTodoData);
+		localStorage.setItem('todoData', JSON.stringify(newTodoData))
   }
 
 	const handleCompleteChange = (id) => {
@@ -15,52 +21,69 @@ export default function List({ todoData, setTodoData }) {
 			}
 			return data;
 		})
-
+  
 		setTodoData(newTodoData);
+		localStorage.setItem('todoData', JSON.stringify(newTodoData))
 	}
 
-	const handleEnd = (result) => {
-		if(!result.destination) return;
-		const newTodoData = todoData;
-
-		const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-		newTodoData.splice(result.destination.index, 0, reorderedItem);
-		setTodoData(newTodoData)
+	const handleEditChange = (event) => {
+		setEditedTitle(event.target.value);
 	}
 
-	return (
-		<div>
-			<DragDropContext onDragEnd={handleEnd}>
-				<Droppable droppableId="todo">
-					{(provided) => (
-						<div {...provided.droppableProps} ref={provided.innerRef}>
-							{todoData.map((data, index) => (
-								<Draggable key={data.id} draggableId={data.id.toString()}
-									index={index}>
-									{(provided, snapshot) => (
-										<div key={data.id} {...provided.draggableProps} ref={provided.innerRef}
-											{...provided.dragHandleProps}
-											className={`${snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'} 
-												flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 
-												border rounded`}>
-											<div className="items-center">
-												<input type="checkbox" defaultChecked={false} 
-													onChange={() => handleCompleteChange(data.id)} />{' '}
-												<span className={data.completed ? 'line-through' : ''}>{data.title}</span>
-											</div>
-											<div className="items-center">
-												<button onClick={() => handleClick(data.id)}
-													className="bx-4 py-2 float-right">x</button>
-											</div>
-										</div>
-									)}
-								</Draggable>
-							))}
-							{provided.placeholder}
-						</div>
-					)}
-				</Droppable>
-			</DragDropContext>
-		</div>
-	)
-}
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		let newTodoData = todoData.map(data => {
+			if(data.id === id) {
+				data.title = editedTitle;
+			}
+			return data;
+		})
+		setTodoData(newTodoData);
+		localStorage.setItem('todoData', JSON.stringify(newTodoData))
+		setIsEditing(false);
+	}
+
+	if(isEditing) {
+		return(
+			<div className={`flex items-center justify-between w-full px-4 py-1 my-2 
+					text-gray-600 border rounded`}>
+				<div className="items-center">
+					<form onSubmit={handleSubmit}>
+						<input value={editedTitle}
+							onChange={handleEditChange}
+							className="w-full px-3 py-2 mr-4 text-gray-500 rounded" />
+					</form>
+				</div>
+				<div className="items-center">
+					<button onClick={() => setIsEditing(false)}
+						className="px-4 py-2 float-right">x</button>
+					<button type="submit" onClick={handleSubmit}
+						className="px-4 py-2 float-right">save</button>
+				</div>
+			</div>
+		)
+	} else {
+		return (
+			<div key={id} {...provided.draggableProps} ref={provided.innerRef}
+				{...provided.dragHandleProps}
+				className={`${snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100'} 
+					flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 
+					border rounded`}>
+				<div className="items-center">
+					<input type="checkbox" defaultChecked={false} 
+						onChange={() => handleCompleteChange(id)} />{' '}
+					<span className={completed ? 'line-through' : ''}>{title}</span>
+				</div>
+				<div className="items-center">
+					<button onClick={() => handleClick(id)}
+						className="px-4 py-2 float-right">x</button>
+					<button onClick={() => setIsEditing(true)}
+						className="px-4 py-2 float-right">edit</button>
+				</div>
+			</div>
+		)
+	}
+});
+
+export default List;
